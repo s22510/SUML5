222eb80426e45392129cf9636d09a9d04cd69448
PiTrzos <Pitrzos@gmail.com>
2023-11-14 11:45:45 +0100

    fixed output visibility

--- main.py
+++ main.py
@@ -8,13 +8,14 @@ model = load_learner(filename)
 
 def main():
     st.set_page_config(page_title="Zadanie 5 - Piotr Trzos")
-    st.title("Zadanie 5 - Piotr Trzos")
+    st.write("Zadanie 5 - Piotr Trzos")
     st.subheader("Cat/Dog Image classifier")
     uploaded_file = st.file_uploader("Choose an image of cat/dog", accept_multiple_files=False, type=["jpg", "png", "JPEG"])
     if uploaded_file:
         st.image(uploaded_file, use_column_width=True)
         prediction = model.predict(uploaded_file.read())[0]
-        st.write(prediction)
+        st.subheader("This is "+prediction)
+        st.write(prediction[1])
 
 
 if __name__ == "__main__":
